a3e2c4b710b8de6ef2c577b2063061ed09a2bfa8
dendriel <vitor.rozsa@hotmail.com>
2013-06-01 12:28:40 -0300

    Changed linked list error enum value.

--- c/llist/src/inc/llist_defines.h
+++ c/llist/src/inc/llist_defines.h
@@ -5,8 +5,8 @@
  *	\b Linked list return codes.
  */
 typedef enum {
-	LLIST_RET_SUCCESS = 0,		//!< Generic success return code.
-	LLIST_RET_ERROR,			//!< Generic error return code.
+	LLIST_RET_SUCCESS	= 0,			//!< Generic success return code.
+	LLIST_RET_ERROR		= -1,			//!< Generic error return code.
 } en_llist_ret_code;
 
 #endif /* LLIST_DEFINES_HEADER */
